69191d36717de99966f96bce2078d9876a5ccac8
Lukas Riegler <lukas.riegler@edu.fh-joanneum.at>
2021-12-09 15:42:17 +0100

    Instruktionen für HowToPlay erstellt und verknüpft

--- app/src/main/java/com/example/rememo/MemoryGame.kt
+++ app/src/main/java/com/example/rememo/MemoryGame.kt
@@ -1,6 +1,9 @@
 package com.example.rememo
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.databinding.GameMemoryBinding
 
@@ -12,5 +15,19 @@ class MemoryGame : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         bindingMemoryGame = GameMemoryBinding.inflate(layoutInflater)
         setContentView(bindingMemoryGame.root)
+
+        bindingMemoryGame.btHowToPlayMemory.setOnClickListener{goToHowToPlayReaction()}
+    }
+
+    private fun goToHowToPlayReaction() {
+
+        val intent: Intent = Intent(this, HowToPlayMemory::class.java)
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext, "Aktivität konnte nicht weitergegeben werden", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/rememo/ReactionGame.kt
+++ app/src/main/java/com/example/rememo/ReactionGame.kt
@@ -3,6 +3,9 @@ package com.example.rememo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.databinding.GameReactionBinding
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.widget.Toast
 
 class ReactionGame : AppCompatActivity(){
 
@@ -12,5 +15,19 @@ class ReactionGame : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         bindingReactionGame = GameReactionBinding.inflate(layoutInflater)
         setContentView(bindingReactionGame.root)
+        
+        bindingReactionGame.btHowToPlayReaction.setOnClickListener{goToHowToPlayReaction()}
     }
+    
+    private fun goToHowToPlayReaction() {
+   
+           val intent: Intent = Intent(this, HowToPlayReaction::class.java)
+   
+           try {
+               startActivity(intent)
+           } catch (e: ActivityNotFoundException) {
+               Toast.makeText(
+                   applicationContext, "Aktivität konnte nicht weitergegeben werden", Toast.LENGTH_LONG).show()
+           }
+       }
 }
