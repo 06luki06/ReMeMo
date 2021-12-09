1c856faa61ee1dc7286b5aed43d2ce0268735340
Lukas Riegler <lukas.riegler@edu.fh-joanneum.at>
2021-12-09 16:44:40 +0100

    credits und support fertig erstellt und verknüpft

--- app/src/main/java/com/example/rememo/Settings.kt
+++ app/src/main/java/com/example/rememo/Settings.kt
@@ -3,6 +3,9 @@ package com.example.rememo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.databinding.SettingsBinding
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.widget.Toast
 
 class Settings : AppCompatActivity(){
 
@@ -13,11 +16,33 @@ class Settings : AppCompatActivity(){
         bindingSettings = SettingsBinding.inflate(layoutInflater)
         setContentView(bindingSettings.root)
 
-        bindingSettings.btSettingsSupport.setOnClickListener{}
-        bindingSettings.btSettingCredits.setOnClickListener{}
+        bindingSettings.btSettingsSupport.setOnClickListener{goToSupport()}
+        bindingSettings.btSettingCredits.setOnClickListener{goToCredits()}
+
+    }
+
+    private fun goToSupport() {
+
+        val intent: Intent = Intent(this, Support::class.java)
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext, "Aktivität konnte nicht weitergegeben werden", Toast.LENGTH_LONG).show()
+        }
     }
 
 
+    private fun goToCredits() {
 
+        val intent: Intent = Intent(this, Credits::class.java)
 
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext, "Aktivität konnte nicht weitergegeben werden", Toast.LENGTH_LONG).show()
+        }
+    }
 }
--- app/src/main/java/com/example/rememo/Support.kt
+++ app/src/main/java/com/example/rememo/Support.kt
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.databinding.SupportBinding
 
@@ -16,20 +17,53 @@ class Support : AppCompatActivity(){
         bindingSupport = SupportBinding.inflate(layoutInflater)
         setContentView(bindingSupport.root)
 
-        bindingSupport.btMailMarkus.setOnClickListener{}
+        bindingSupport.btMailMarkus.setOnClickListener{sendMailToMarkus()}
+        bindingSupport.btMailVanessa.setOnClickListener{sendMailToVanessa()}
+        bindingSupport.btMailLukas.setOnClickListener{sendMailToLukas()}
     }
 
-    private fun openMail(){
-        val mailIntent: Intent = Uri.parse("mailto:lukas.riegler@napalmrecords.com").let {
+    private fun sendMailToMarkus(){
+        val mailIntent: Intent = Uri.parse("mailto:markus.almer@edu.fh-joanneum.at").let {
             Intent(Intent.ACTION_SENDTO, it).apply {
-                putExtra(Intent.EXTRA_SUBJECT, "Testnachricht")
+                putExtra(Intent.EXTRA_SUBJECT, "SUPPORT-Anfrage ReMeMo")
             }
         }
         
         try {
             startActivity(mailIntent)
         }catch (e: ActivityNotFoundException){
-            //Feedback to User
+            Toast.makeText(
+                applicationContext, "Mail konnte nicht gesendet werden", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun sendMailToVanessa(){
+        val mailIntent: Intent = Uri.parse("mailto:vanessa.riedl@edu.fh-joanneum.at").let {
+            Intent(Intent.ACTION_SENDTO, it).apply {
+                putExtra(Intent.EXTRA_SUBJECT, "SUPPORT-Anfrage ReMeMo")
+            }
+        }
+
+        try {
+            startActivity(mailIntent)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(
+                applicationContext, "Mail konnte nicht gesendet werden", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun sendMailToLukas(){
+        val mailIntent: Intent = Uri.parse("mailto:lukas.riegler@edu.fh-joanneum.at").let {
+            Intent(Intent.ACTION_SENDTO, it).apply {
+                putExtra(Intent.EXTRA_SUBJECT, "SUPPORT-Anfrage ReMeMo")
+            }
+        }
+
+        try {
+            startActivity(mailIntent)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(
+                applicationContext, "Mail konnte nicht gesendet werden", Toast.LENGTH_LONG).show()
         }
     }
 }
