77155f890b122624d2f945e78bd06980e36b053d
Lukas Riegler <lukas.riegler@edu.fh-joanneum.at>
2021-12-09 11:54:03 +0100

    zweiter upload - startseite und spielauswahl layout fertig

--- app/src/main/java/com/example/rememo/MainActivity.kt
+++ app/src/main/java/com/example/rememo/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.rememo
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.rememo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iBGoToPlayChoice.setOnClickListener{goToGames()}
+       
+    }
+
+    private fun goToGames() {
+
+        val intent: Intent = Intent(this, GameChoice::class.java)
+
+
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                applicationContext, "Aktivität konnte nicht weitergegeben werden", Toast.LENGTH_LONG).show()
+        }
     }
 }
