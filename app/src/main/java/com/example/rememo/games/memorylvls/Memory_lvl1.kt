0e7022854f5b12d1a4c83fa6eadc5dca1136d953
Lukas Riegler <lukas.riegler@edu.fh-joanneum.at>
2021-12-22 09:11:21 +0100

    nix aufd reih brocht

--- app/src/main/java/com/example/rememo/games/memorylvls/Memory_lvl1.kt
+++ app/src/main/java/com/example/rememo/games/memorylvls/Memory_lvl1.kt
@@ -3,6 +3,7 @@ package com.example.rememo.games.memorylvls
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.databinding.MemoryLvl1Binding
@@ -25,6 +26,15 @@ class Memory_lvl1 : AppCompatActivity() {
         gameEngine.countDown(bindingMemorylvl1, countDown)
         val intent: Intent = Intent(this, Pause::class.java)
         bindingMemorylvl1.iBPauseScreen.setOnClickListener { goToPause(intent) }
+
+        val button_1 = bindingMemorylvl1.btMemoryLv111
+        val button_2 = bindingMemorylvl1.btMemoryLv112
+        val button_3 = bindingMemorylvl1.btMemoryLv113
+        val button_4 = bindingMemorylvl1.btMemoryLv114
+
+        val buttonArray = arrayListOf<Button>(button_1, button_2, button_3, button_4)
+        gameEngine.selectInput(buttonArray, buttonChoice)
+
     }
 
     fun goToPause(intent: Intent) {
