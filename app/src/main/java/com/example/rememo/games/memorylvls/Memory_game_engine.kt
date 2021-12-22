5f5033ed007eedec93d8fdee9214e4bca27893e2
Lukas Riegler <lukas.riegler@edu.fh-joanneum.at>
2021-12-22 11:08:26 +0100

    Input des Spiels wurde vervollständigt

--- app/src/main/java/com/example/rememo/games/memorylvls/Memory_game_engine.kt
+++ app/src/main/java/com/example/rememo/games/memorylvls/Memory_game_engine.kt
@@ -2,6 +2,7 @@ package com.example.rememo.games.memorylvls
 
 import android.os.CountDownTimer
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rememo.R
 import com.example.rememo.databinding.MemoryLvl1Binding
@@ -10,6 +11,9 @@ class Memory_game_engine : AppCompatActivity() {
     private lateinit var countDownTimer: CountDownTimer
     private val interval: Long = 1000
     private var result: String = ""
+    private var timerEnd :Boolean = false
+
+
 
     private fun selectRandomButtons(buttonchoice: Array<String>): ArrayList<String> {
         var randomNumber: Int
@@ -33,19 +37,14 @@ class Memory_game_engine : AppCompatActivity() {
     }
 
     fun countDown(bindingMemorylvl1: MemoryLvl1Binding, countDownTimerLength: Long) {
+        timerEnd = false
         bindingMemorylvl1.tVMemoryLv1Numbers.text = result
         countDownTimer = object : CountDownTimer(countDownTimerLength, interval) {
             override fun onTick(millisUntilFinished: Long) {}
             override fun onFinish() {
                 bindingMemorylvl1.tVMemoryLv1Numbers.setText(R.string.memory_its_your_turn)
+
             }
         }.start()
-    }
-
-    fun selectInput(buttons: ArrayList<Button>, buttonName: Array<Button>) {
-
-        for (i in 0..buttons.size - 1) {
-
-        }
     }
 }
